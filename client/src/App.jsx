7c6eefc4f2f66757769dc9f5ff0295fae1b88ded
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-11-19 20:14:08 +0200

    Init project with header and nav components

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from './components/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+
+    </>
+  )
+}
+
+export default App
