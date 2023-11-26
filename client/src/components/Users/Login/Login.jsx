1893f307da185f7a26f4852e31cf305ff0f8cac4
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-11-26 23:48:14 +0200

    Change login and register to controlled forms

--- client/src/components/Users/Login/Login.jsx
+++ client/src/components/Users/Login/Login.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 import "./Login.css"
 
+import useForm from "../../../hooks/useForm.js";
+
+
 const Login = () => {
+    const {values, onChange, onSubmit} = useForm({
+        email: "",
+        password: "",
+    });
+
     return (
-        <form className="login-cover">
+        <form className="login-cover" onSubmit={onSubmit}>
             <h1>Login</h1>
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input type="email" id="email" name="email" placeholder="Email" onChange={onChange} value={values.email}/>
+            <input type="password" id="password" name="password" placeholder="Password" onChange={onChange} value={values.password}/>
 
             <button className="login-btn">Login</button>
 
--- client/src/components/Users/Register/Register.jsx
+++ client/src/components/Users/Register/Register.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import "./Register.css"
 
+import useForm from "../../../hooks/useForm.js";
+
+
 const Register = () => {
-    return(
-        <form className="register-cover">
+    const {values, onChange, onSubmit} = useForm();
+
+    return (
+        <form className="register-cover" onSubmit={onSubmit}>
             <h1 className="register">Register</h1>
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="rePass" placeholder="Confirm password" />
+            <input type="email" id="email" name="email" placeholder="Email" onChange={onChange} value={values.email}/>
+            <input type="password" id="password" name="password" placeholder="Password" onChange={onChange} value={values.name}/>
+            <input type="rePass" id="rePass" name="rePass" placeholder="Confirm password" onChange={onChange} value={values.rePass}/>
 
             <button className="register-btn" type="submit">Register</button>
 
