a6c4986c8c3e31889885d42b9171eb6dcf7fb6c6
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-11-26 23:22:48 +0200

    Add register form

--- client/src/components/Users/Register/Register.jsx
+++ client/src/components/Users/Register/Register.jsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
+import "./Register.css"
+
 const Register = () => {
     return(
-        <h1>Register</h1>
+        <form className="cover">
+            <h1 className="register">Register</h1>
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="rePass" placeholder="Confirm password" />
+
+            <button className="register-btn" type="submit">Register</button>
+
+            <Link className="text" to="/users/login">Already have an account?</Link>
+        </form>
     )
 }
 
