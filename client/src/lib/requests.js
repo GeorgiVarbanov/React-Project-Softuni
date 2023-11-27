d2a8cf1ed876ba5112668bc7c19be4fa3b281d1a
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-11-27 21:46:59 +0200

    Finishing up requester

--- client/src/lib/requests.js
+++ client/src/lib/requests.js
@@ -13,7 +13,7 @@ const buildOptions = (data) => {
 }
 
 
-export const request = async (method, url, data) => {
+const request = async (method, url, data) => {
 
 
 
@@ -30,5 +30,12 @@ export const request = async (method, url, data) => {
 
     return result;
 };
+
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const del = request.bind(null, "DELETE");
+export const patch = request.bind(null, "PATCH");
+
 
 
