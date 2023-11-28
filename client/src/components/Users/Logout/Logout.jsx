a45d22c1d4c81d068e6a94aceae718254ac4df6e
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-11-29 00:06:38 +0200

    Logout component

---
+++ client/src/components/Users/Logout/Logout.jsx
@@ -0,0 +1,19 @@
+import { useContext, useEffect } from "react";
+
+import * as authService from "../../../services/authService.js"
+import { useNavigate } from "react-router-dom";
+import paths from "../../../paths.js";
+import AuthContext from "../../../contexts/authContext.js";
+
+export default function Logout () {
+    const {logoutHandler} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        authService.logout()
+        .then(() => logoutHandler())
+        .catch(() => navigate(paths.home))
+    }, [])
+
+    return null;
+}
