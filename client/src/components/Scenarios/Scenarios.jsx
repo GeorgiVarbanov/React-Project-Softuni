45648f3221c4568cd9adae449813d5a45dd35ca0
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-05 01:32:18 +0200

    Adding basic component layout

--- client/src/components/Scenarios/Scenarios.jsx
+++ client/src/components/Scenarios/Scenarios.jsx
@@ -1,7 +1,67 @@
+import { Link } from "react-router-dom";
+import "./Scenarios.css";
+
 const Scenarios = () => {
     return (
-        <h1>Scenarios</h1>
+        <section className="scenario-section">
+            <div className="table-row">
+                <Link className="table-cell">
+                    <div className="table-cell-content">
+                        <div className="scenario-listing-item-avatar"></div>
+                        <div className="sources-listing-item-title">Title</div>
+                    </div>
+                </Link>
+            </div>
+            <div className="table-row">
+                <Link className="table-cell">
+                    <div className="table-cell-content">
+                        <div className="scenario-listing-item-avatar"></div>
+                        <div className="sources-listing-item-title">Title</div>
+                    </div>
+                </Link>
+            </div>
+            <div className="table-row">
+                <Link className="table-cell">
+                    <div className="table-cell-content">
+                        <div className="scenario-listing-item-avatar"></div>
+                        <div className="sources-listing-item-title">Title</div>
+                    </div>
+                </Link>
+            </div>
+            <div className="table-row">
+                <Link className="table-cell">
+                    <div className="table-cell-content">
+                        <div className="scenario-listing-item-avatar"></div>
+                        <div className="sources-listing-item-title">Title</div>
+                    </div>
+                </Link>
+            </div>
+            <div className="table-row">
+                <Link className="table-cell">
+                    <div className="table-cell-content">
+                        <div className="scenario-listing-item-avatar"></div>
+                        <div className="sources-listing-item-title">Title</div>
+                    </div>
+                </Link>
+            </div>
+            <div className="table-row">
+                <Link className="table-cell">
+                    <div className="table-cell-content">
+                        <div className="scenario-listing-item-avatar"></div>
+                        <div className="sources-listing-item-title">Title</div>
+                    </div>
+                </Link>
+            </div>
+            <div className="table-row">
+                <Link className="table-cell">
+                    <div className="table-cell-content">
+                        <div className="scenario-listing-item-avatar"></div>
+                        <div className="sources-listing-item-title">Title</div>
+                    </div>
+                </Link>
+            </div>
+        </section>
     )
 }
 
-export default  Scenarios;
+export default Scenarios;
