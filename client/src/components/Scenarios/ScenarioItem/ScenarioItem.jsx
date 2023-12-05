8694d56c53e87143b1a8491d838352786deb4c95
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-05 23:52:18 +0200

    Dynamicly load db data for single item

--- client/src/components/Scenarios/ScenarioItem/ScenarioItem.jsx
+++ client/src/components/Scenarios/ScenarioItem/ScenarioItem.jsx
@@ -1,20 +1,27 @@
 import { Link } from "react-router-dom";
 import "./ScenarioItem.css";
 
-const ScenarioItem = () => {
+const ScenarioItem = ({
+    _id,
+    campaign,
+    imageUrl,
+}) => {
 
+    const avatarStyle = {
+        backgroundImage: `url(${imageUrl})`, // Set the background image dynamically
+    };
 
     return (
         <div className="table-row">
-            <Link className="table-cell">
-                <div className="table-cell-content">
-                    <div className="scenario-listing-item-avatar"></div>
-                    <div className="sources-listing-item-title">Title</div>
+            <Link to={`/scenarios/${_id}`} className="table-cell">
+                <div className="table-cell-content" >
+                    <div className="scenario-listing-item-avatar" style={avatarStyle}></div>
+                    <div className="sources-listing-item-title">{campaign}</div>
                 </div>
             </Link>
         </div>
     );
-}
+};
 
 export default ScenarioItem;
 
