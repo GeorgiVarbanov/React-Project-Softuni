d5369d6bb5937d18c62ed52a8205adf050f7ff5b
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-10 01:57:04 +0200

    Update css and fix links

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Footer.css"
 
 //FIX CSS AND LINKS
@@ -10,18 +11,17 @@ const Footer = () => {
                     <div className="footer-col">
                         <h4>Useful links</h4>
                         <ul>
-                            <li><a href="#">about us</a></li>
-                            <li><a href="#">This will be a link to basic DND rules</a></li>
-                            <li><a href="#">privacy policy</a></li>
+                            <li><Link to="/about">about us</Link></li>
+                            <li><a href="https://www.dndbeyond.com/sources/basic-rules">Basic Rules</a></li>
+                            <li><a href="https://www.dndbeyond.com/sources/dmg">Dungeon master guide</a></li>
                         </ul>
                     </div>
                     <div className="footer-col">
                         <h4>follow us</h4>
                         <div className="social-links">
-                            <a href="#"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#"><i className="fab fa-twitter"></i></a>
-                            <a href="#"><i className="fab fa-instagram"></i></a>
-                            <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                            <a href="https://www.facebook.com/georgi.vurbanov.3/"><i className="fab fa-facebook-f"></i></a>
+                            <a href="https://www.instagram.com/gvurbanov/"><i className="fab fa-instagram"></i></a>
+                            <a href="https://www.linkedin.com/in/георги-върбанов-368977193"><i className="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                 </div>
