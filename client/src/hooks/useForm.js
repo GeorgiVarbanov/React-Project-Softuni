3ccffb24ce90a582c5edc6766b83a174fe138408
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-09 16:52:46 +0200

    Add input validation before making request

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
-export default function useForm(submitHandler, initialValues){
+export default function useForm(submitHandler, initialValues) {
     const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
 
     const onChange = (e) => {
         setValues(state => ({
@@ -13,11 +14,47 @@ export default function useForm(submitHandler, initialValues){
     const onSubmit = (e) => {
         e.preventDefault();
 
-        submitHandler(values);
+        console.log(Object.keys(values))
+
+        const valueKeys = Object.keys(values);
+        const validationErrors = {}
+
+        valueKeys.forEach(key => {
+            if (!values[key].trim()) {
+                validationErrors[key] = `${key} is required`;
+            } else {
+                switch (key) {
+                    case 'email':
+                        if (!/\S+@\S+\.\S+/.test(values[key])) {
+                            validationErrors[key] = `Invalid ${key}`;
+                        }
+                        break;
+                    case 'password':
+                        if (values[key].length < 6) {
+                            validationErrors[key] = `${key} should be at least 6 characters`;
+                        }
+                        break;
+                    case 'rePass':
+                        if (values.password !== values.rePass) {
+                            validationErrors[key] = `Password mismatch`;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        setErrors(validationErrors);
+
+        if (Object.keys(validationErrors).length === 0) {
+            submitHandler(values);
+        }
     }
 
-    return{
+    return {
         values,
+        errors,
         onChange,
         onSubmit,
     }
