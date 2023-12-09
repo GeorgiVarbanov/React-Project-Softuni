2ba1e7020fc69a4fb24f844dd7077d50870aae65
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-09 02:29:08 +0200

    Load dynamic content

--- client/src/components/Users/Profile/Profile.jsx
+++ client/src/components/Users/Profile/Profile.jsx
@@ -1,9 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+
 import "./Profile.css";
 
 import AuthContext from "../../../contexts/authContext.jsx";
+import ProfileCard from "./ProfileCard/ProfileCard.jsx";
+import * as scenarioService from "../../../services/scenarioService.js";
 
 const Profile = () => {
+
+    const [ownedScenarios, setOwnedScenarios] = useState([]);
+
+    useEffect(() => {
+        scenarioService.getAll()
+            .then(result => setOwnedScenarios(result))
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
 
     const { email, username } = useContext(AuthContext);
     return (
@@ -17,9 +30,16 @@ const Profile = () => {
             </div>
             <div className="right">
                 <div className="created-scenarios">
-                    <h3>Scenarios</h3>
+                    <h3 className="h3-title">Scenarios</h3>
                     <div className="scenarios-data">
 
+                        {ownedScenarios.map(scenario => (
+                            <ProfileCard key={scenario._id} {...scenario} />
+                        ))}
+
+                        {ownedScenarios.length === 0 && (
+                            <h3 className="no-articles-profile">No articles yet</h3>
+                        )}
                     </div>
                 </div>
             </div>
