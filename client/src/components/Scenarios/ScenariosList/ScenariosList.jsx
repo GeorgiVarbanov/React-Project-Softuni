8c7e44fbc3055065cb3d1232e276617d08249f6b
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-09 16:54:47 +0200

    Renaming component

---
+++ client/src/components/Scenarios/ScenariosList/ScenariosList.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import "./ScenariosList.css";
+import * as scenarioService from "../../../services/scenarioService.js";
+import ScenarioItem from "../ScenarioItem/ScenarioItem.jsx";
+
+
+const Scenarios = () => {
+
+    const [scenarios, setScenarios] = useState([]);
+
+    useEffect(() => {
+        scenarioService.getAll()
+            .then(result => setScenarios(result))
+            .catch(err => {
+                alert(err.message);
+            })
+    }, [])
+    
+    return (
+        <section className="scenario-section">
+            {scenarios.map(scenario => (
+                <ScenarioItem key={scenario._id} {...scenario} />
+            ))}
+
+            {scenarios.length === 0 && (
+                <h3 className="no-articles">No articles yet</h3>
+            )}
+        </section>
+    )
+}
+
+export default Scenarios;
