ee5b9e59e101e3591ea72ffda4203baaa9845246
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-09 23:20:51 +0200

    add comment section and css for it

--- client/src/components/Scenarios/ScenarioDetails/ScenarioDetails.jsx
+++ client/src/components/Scenarios/ScenarioDetails/ScenarioDetails.jsx
@@ -31,23 +31,46 @@ const ScenarioDetails = () => {
     }
 
     return (
-        <section className="details-section">
-            <div className="scenario-details-item-avatar" style={avatarStyle}></div>
-            <div className="scenario-details-info">
+        <>
+            <section className="details-section">
+                <div className="scenario-details-item-avatar" style={avatarStyle}></div>
+                <div className="scenario-details-info">
 
-                <div className="scenario-details-item-title">Campaign name: {scenario.campaign}</div>
-                <div className="scenario-details-item-levels">{scenario.level}</div>
-                <div className="scenario-detauls-description-header">Description: </div>
-                <p className="scenario-details-item-description">{scenario.description}</p>
+                    <div className="scenario-details-item-title">Campaign name: {scenario.scenario}</div>
+                    <div className="scenario-details-item-levels">{scenario.level}</div>
+                    <div className="scenario-detauls-description-header">Description: </div>
+                    <p className="scenario-details-item-description">{scenario.description}</p>
 
-                {userId === scenario._ownerId && (
-                    <div className="scenario-details-buttons">
-                        <Link className="edit-btn" to={`/scenarios/${scenario._id}/edit`}>Edit</Link>
-                        <button className="delete-btn" onClick={deleteButtonClickHandler}>Delete</button>
-                    </div>
-                )}
-            </div>
-        </section>
+                    {userId === scenario._ownerId && (
+                        <div className="scenario-details-buttons">
+                            <Link className="edit-btn" to={`/scenarios/${scenario._id}/edit`}>Edit</Link>
+                            <button className="delete-btn" onClick={deleteButtonClickHandler}>Delete</button>
+                        </div>
+                    )}
+                </div>
+            </section>
+
+            <section className="comments-section">
+                <div className="comment-header">Comments:</div>
+                <div className="comments">
+                    <ul>
+                        <li>Hello there</li>
+                        <li>Hello there</li>
+                    </ul>
+                </div>
+            </section>
+            <section className="add-comment">
+                <form className="comment-form">
+                    <textarea className="comment-input"
+                        type="comment"
+                        id="comment"
+                        name="comment"
+                        placeholder="Comment"
+                    />
+                    <button className="comment-btn">Add comment</button>
+                </form>
+            </section>
+        </>
     );
 }
 
