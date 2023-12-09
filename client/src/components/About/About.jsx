cf060300166cb20580b9d42a257b58f7f3a02d8d
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-12-09 16:54:19 +0200

    Fix broken css for title

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -12,30 +12,30 @@ const About = () => {
             <h1 className="team-h1">Meet the team!</h1>
             <div className="about-card-holder">
                 
-                <div className="card-container">
+                <div className="card-container-about">
                     <img 
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAZEucVqvlhR008vW6LrmiTLaPusSqAH4W5PlR7tmuXl0NTe4O3VKEz5uYrIxFS10sd4M&usqp=CAU" 
                     alt="" 
                     className="card-img"/>
-                    <h1 className="card-title">Liam Emberforge</h1>
+                    <h1 className="card-title-about">Liam Emberforge</h1>
                     <p className="role">Community Bard</p>
                     <p className="card-description">Heart and soul, fostering camaraderie among D&D enthusiasts.</p>
                 </div>
-                <div className="card-container">
+                <div className="card-container-about">
                     <img 
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKjmv17ojit1vJBB-LtzZx5FkaPfttQ1sJt4sNriy83Ha6ziJpiLsISfpQm0u-lOcGLHg&usqp=CAU" 
                     alt="" 
                     className="card-img"/>
-                    <h1 className="card-title">Gideon Stoneshield</h1>
+                    <h1 className="card-title-about">Gideon Stoneshield</h1>
                     <p className="role">Tech-savvy Webmaster</p>
                     <p className="card-description">Graphic designer conjuring visually stunning D&D magic.</p>
                 </div>
-                <div className="card-container">
+                <div className="card-container-about">
                     <img 
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQziJ01cM5F19iipbdwu7vp28oV_6-df49NBA&usqp=CAU" 
                     alt="" 
                     className="card-img"/>
-                    <h1 className="card-title">Elena Darkmoon</h1>
+                    <h1 className="card-title-about">Elena Darkmoon</h1>
                     <p className="role">Dungeon Master Extraordinaire</p>
                     <p className="card-description">Creative storyteller weaving unforgettable D&D adventures.</p>
                 </div>
